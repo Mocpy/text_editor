c9072fe37bd11e7f8c96facedf183d5dc4cb3d62
Mocpy <mocpy@seznam.cz>
2023-10-13 17:37:31 +0200

    added menubar

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 import tkinter as tk
 from tkinter import filedialog
 
-
 def save_text():
     text = text_area.get("1.0", "end-1c")
     file_path = filedialog.asksaveasfilename(defaultextension=".txt")
@@ -11,16 +10,38 @@ def save_text():
         with open(file_path, "w") as file:
             file.write(text)
 
+def save_button():
+    save_button = tk.Button(root, text="Uložit", command=save_text)
+    save_button.pack()
+
+class Menues:
+    def menues(self):
+    
+        menubar = tk.Menu(root)
+        FileMenu = tk.Menu(menubar, tearoff=0)
+        HelpMenu = tk.Menu(menubar, tearoff=0)
+
+        menubar.add_cascade(label="File", menu=FileMenu)
+
+        menubar.add_cascade(label="Nápověda", menu=HelpMenu)
+
+        FileMenu.add_command(label="Save", command=save_text)
+        
+
+        root.config(menu=menubar)
+
+
+
 
 root = tk.Tk()
 root.title("Uložit jako .txt")
 root.geometry("400x410")
 
-save_button = tk.Button(root, text="Uložit", command=save_text)
-save_button.pack()
+
 
 text_area = tk.Text(root)
 text_area.pack()
 
+Menues.menues('self')
 
 root.mainloop()
