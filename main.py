7a84685066af9c0cf45911a0b2b13d52722d5cc9
Mocpy <mocpy@seznam.cz>
2023-10-26 14:10:18 +0200

    Now You can use save and save as

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
 import tkinter as tk
 from tkinter import filedialog
 import os
@@ -17,6 +15,16 @@ class TextField:
     def __init__(self, root):
         self.text_field = tk.Text(root)
         self.text_field.pack(fill=tk.BOTH, expand=True)
+        self.file_path = None  # Variable to store the file path
+
+    def save_file_without_dialog(self):
+        if self.file_path:  # If a file path exists, save the file without the dialog
+            text = self.text_field.get("1.0", "end-1c")
+            with open(self.file_path, "w") as file:
+                file.write(text)
+
+    def get_text(self):
+        return self.text_field.get("1.0", "end-1c")
 
 
 text_field = TextField(root)
@@ -24,28 +32,45 @@ text_field = TextField(root)
 
 def save_file(event=None):
     # Get the text from the text field
-    text = text_field.text_field.get("1.0", "end-1c")
-    # Ask the user for a file path to save
-    file_path = filedialog.asksaveasfilename(defaultextension=".txt", initialdir=default_path)
-    if file_path:   # If a file path is selected, open the file in write mode and write the text to it
-        with open(file_path, "w") as file:
+    text = text_field.get_text()
+    # Check if a file path exists
+    if text_field.file_path:
+        # Save the file without the dialog
+        with open(text_field.file_path, "w") as file:
             file.write(text)
+    else:
+        # Show the save dialog
+        file_path = filedialog.asksaveasfilename(defaultextension=".txt", initialdir=default_path)
+        if file_path:
+            with open(file_path, "w") as file:
+                file.write(text)
+                text_field.file_path = file_path
+
+
+def save_file_as(event=None):
+    if text_field.file_path:  # If a file path exists, save the file without the dialog
+        text_field.save_file_without_dialog()
+    else:  # If no file path exists, show the save dialog
+        text = text_field.get_text()
+        file_path = filedialog.asksaveasfilename(defaultextension=".txt", initialdir=default_path)
+        if file_path:
+            with open(file_path, "w") as file:
+                file.write(text)
+            text_field.file_path = file_path
 
 
 def open_file(event=None):
-    # Define the file types to be displayed in the open file dialog
     filetypes = (
         ('text files', '*.txt'),
         ('All files', '*.*')
     )
-    # Show the open file dialog and get the selected file path
     file_path = filedialog.askopenfilename(filetypes=filetypes, initialdir=default_path)
     if file_path:
-        # If a file path is selected, read the file and display its content in the text field
         with open(file_path, "r") as file:
             content = file.read()
-            text_field.text_field.delete('1.0', tk.END)  # Clear existing text
+            text_field.text_field.delete('1.0', tk.END)
             text_field.text_field.insert('1.0', content)
+        text_field.file_path = file_path
 
 
 class Menus:
@@ -53,32 +78,23 @@ class Menus:
         self.menus()
 
     def menus(self):
-        menubar = tk.Menu(root)    # Create the menubar
-        # Create the file and Appearance menus
+        menubar = tk.Menu(root)
         filemenu = tk.Menu(menubar, tearoff=0)
         apperance = tk.Menu(menubar, tearoff=0)
-
-        # Add the file and Appearance menus to the menubar
         menubar.add_cascade(label="File", menu=filemenu)
         menubar.add_cascade(label="Appearance", menu=apperance)
-
-        # Add commands to the file menu
-        filemenu.add_command(label="Save", command=save_file)
         filemenu.add_command(label='Open', command=open_file)
-
-        # Configure the root window to use the menubar
+        filemenu.add_command(label="Save", command=save_file)
+        filemenu.add_command(label='Save as', command=save_file_as)
         root.config(menu=menubar)
 
 
 class Binds:
     def __init__(self):
-        root.bind("<Control-s>", save_file)  # Ctrl + S to save file
-        root.bind("<Control-o>", open_file)  # Ctrl + O to open file
+        root.bind("<Control-s>", save_file)
+        root.bind("<Control-o>", open_file)
 
 
-binds = Binds()    # Create an instance of the Binds class
-menus = Menus()    # Create an instance of the Menus class
-
-
-# Start the main event loop
+binds = Binds()
+menus = Menus()
 root.mainloop()
